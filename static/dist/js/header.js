9b6f0350d8f67291f9243ec07242b02d357b4cba
liuxuejin <liuxuejin008@gmail.com>
2024-06-11 15:10:31 +0800

    提交auth测试

--- static/dist/js/header.js
+++ static/dist/js/header.js
@@ -16,76 +16,12 @@ window.addEventListener('resize', function () {
   }
 })
 
+const authSm = document.querySelector('.auth-sm')
+const loginSm = authSm.querySelector('.login-btn')
+const loginBtn = document.querySelector('.login-btn')
 
-auth0.createAuth0Client({
-  domain: "dev-b35cc3imumqh8e5w.us.auth0.com",
-  clientId: "2ur3dAMCqtRDwBht864h4SkcS30Oaebe",
-  authorizationParams: {
-    redirect_uri: window.location.origin + '/main.html'
-  }
-}).then(async (auth0Client) => {
-  // login
-  const authSm = document.querySelector('.auth-sm')
-  const userSm = authSm.querySelector('.user')
-  const loginSm = authSm.querySelector('.login-btn')
-
-  const loginSmShowClass = 'inline-flex'
-  const userSmShowClass = 'flex' 
-  const showClass = 'md:flex'
-
-  const userWarper = document.querySelector('.user-warper')
-  const loginBtn = document.querySelector('.login-btn')
-  const user = userWarper.querySelector('.user');
-
-  function setNameAndAvatar(user, userProfile) {
-    user.querySelector('img').src = userProfile.picture;
-    user.querySelector('.user-name').textContent = userProfile.name;
-  }
-
-  function onAuth(e) {
-    e.preventDefault();
-    auth0Client.loginWithRedirect({
-      authorizationParams: {
-        redirect_uri: window.location.origin + '/main.html'
-      }
-    });
-  }
-
-  loginSm.addEventListener('click', onAuth)
-  loginBtn.addEventListener('click', onAuth)
-
-
-  if (location.search.includes("state=") &&
-    (location.search.includes("code=") ||
-      location.search.includes("error="))) {
-    try {
-      await auth0Client.handleRedirectCallback();
-      window.history.replaceState({}, document.title, location.pathname);
-    } catch (e) {
-      alert(e.message)
-      console.error(e);
-    }
-  }
-
-
-  const isAuthenticated = await auth0Client.isAuthenticated();
-  const userProfile = await auth0Client.getUser();
-
-  if (isAuthenticated) {
-    loginSm.classList.add('hidden')
-    loginBtn.classList.remove(showClass)
-    userSm.classList.remove('hidden')
-    userSm.classList.add(userSmShowClass)
-    setNameAndAvatar(userSm, userProfile)
-    setNameAndAvatar(user, userProfile)
-    userWarper.classList.add(showClass)
-  } else {
-    debugger
-    userSm.classList.add('hidden')
-    userSm.classList.remove(userSmShowClass)
-    loginSm.classList.remove('hidden')
-    loginSm.classList.add(loginSmShowClass)
-    loginBtn.classList.add(showClass)
-    loginBtn.classList.remove('hidden')
-  }
-})
+function onAuth(e) {
+  location.href = '/login'
+}
+loginSm.addEventListener('click', onAuth)
+loginBtn.addEventListener('click', onAuth)
